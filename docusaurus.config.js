6bef55f44040e67f2c2ee492cc2d0e312fda54cc
gabrielpedro06 <gabi.pedro2006@gmail.com>
2025-05-16 11:06:08 +0100

    github repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'gabrielpedro06', // Usually your GitHub org/user name.
-  projectName: 'doc', // Usually your repo name.
+  projectName: 'Docussaurus', // Usually your repo name.
   trailingSlash: false,
   deploymentBranch: 'gh-pages',
 
