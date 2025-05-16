e121f64d2f2d78363676890f8a6cf09cc3f606a3
gabrielpedro06 <gabi.pedro2006@gmail.com>
2025-05-16 10:54:58 +0100

    nome do site alterado

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Doc Site!!',
+  title: 'Docusaurs Site!!',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
